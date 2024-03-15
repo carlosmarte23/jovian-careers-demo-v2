406ae2cbf56316a06b3280630f40f2094c63095b
Carlos <carlosmarte23@gmail.com>
2024-03-14 21:14:04 -0400

    Basic Html structure of the home page

--- app.py
+++ app.py
@@ -2,9 +2,24 @@ from flask import Flask, render_template
 
 app = Flask(__name__)
 
+JOBS = [
+  {
+    "id": 1,
+    "title": "Software Engineer",
+    "location": "Mountain View",
+    "salary": "Rs. 10,00,000",
+  },
+  {
+    "id": 2,
+    "title": "Data Scientist",
+    "location": "Delhi, India",
+    "salary": "Rs. 15,00,000",
+  }
+]
+
 @app.route("/")
 def hello():
-    return render_template("home.html")
+    return render_template("home.html", jobs=JOBS)
 
 if __name__ == "__main__":
     app.run(host='0.0.0.0', debug=True)

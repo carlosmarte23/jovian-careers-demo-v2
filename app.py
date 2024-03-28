fa8bba9de53d78c764fbe2b1b35301ea879e9f5c
Carlos <carlosmarte23@gmail.com>
2024-03-27 20:05:27 -0400

    add individual route for each job

--- app.py
+++ app.py
@@ -1,5 +1,5 @@
 from flask import Flask, render_template, jsonify
-from database import load_jobs_from_db
+from database import load_jobs_from_db, load_job_from_db
 
 app = Flask(__name__)
 
@@ -11,8 +11,12 @@ def hello():
 @app.route("/api/jobs")
 def get_jobs():
   jobs = load_jobs_from_db()
-  results = [dict(r) for r in jobs]
-  return jsonify(results)
+  return jsonify(jobs)
+
+@app.route("/job/<id>")
+def show_job(id):
+  job = load_job_from_db(id)
+  return jsonify(job)
 
 if __name__ == "__main__":
     app.run(host='0.0.0.0', debug=True)
--- database.py
+++ database.py
@@ -11,5 +11,13 @@ def load_jobs_from_db():
     result = conn.execute(text("select * from jobs"))
     jobs = []
     for row in result.all():
-      jobs.append(row._mapping)
+      jobs.append(dict(row._mapping))
     return jobs
+  
+def load_job_from_db(id):
+  with engine.connect() as conn:
+    result = conn.execute(text("select * from jobs where id = :val"), {"val": id})
+    rows = result.all()
+    if len(rows) == 0:
+      return None
+    return dict(rows[0]._mapping)
